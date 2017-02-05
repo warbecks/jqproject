81fccc3894ffa32715eeea7dce64ea3aa04d81bc
warbecks <warbecks@gmail.com>
2017-02-05 07:59:07 -0500

    Add files via upload

---
+++ application.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	for(var i = 0; i < 16; i++) {
+	for(var y = 0; y < 16; y++) {
+		var square = $("<div class="square"></div>");
+ 		square.appendTo('#container');
+}
+  }
+});
